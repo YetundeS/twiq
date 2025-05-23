386321149359043bda0002d7a0ffd3b4d49c56f1
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2025-05-23 19:44:21 +0100

    fixed linkedin businesss model, ready for backend integration

--- src/app/platform/[slug]/linkedin-business/page.jsx
+++ src/app/platform/[slug]/linkedin-business/page.jsx
@@ -1,46 +1,67 @@
 "use client";
 
-import { useSideBar } from "@/store/sidebarStore";
-import './lb.css';
-import useAuthStore from "@/store/authStore";
-import { useEffect } from "react";
-import { useHydrationZustand } from "@codebayu/use-hydration-zustand";
+import useLIB from "@/hooks/useLIB";
+import "./lb.css";
 import { PanelRightOpen, SquarePen } from "lucide-react";
+import ChatInputArea from "@/components/carouselComponents/chatInputArea";
+import ChatMessageWindow from "@/components/carouselComponents/chatMessageWindow";
+import ModelName from "@/components/modelsComponent/modelName";
 
 const LinkedInBusinessModel = () => {
-  const { isSidebarOpen, setIsSidebarOpen } = useSideBar();
-
-  const { user } = useAuthStore();
-
-  const isHydrated = useHydrationZustand(useAuthStore);
-
-  useEffect(() => {
-    if (isHydrated && !user) { 
-      router.push("/auth");
-    }
-  }, [user, isHydrated]);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
+  const {
+    isSidebarOpen,
+    toggleSidebar,
+    modelName,
+    modelDescription,
+    inputValue,
+    setInputValue,
+    sendMessage,
+    closeStreaming,
+    streamingData,
+    streaming,
+    sendBtnActive,
+    chats,
+    messagesEndRef,
+    aiSuggestions,
+  } = useLIB();
 
   return (
-        <div className="lb-page_content">
-          <div className="lb-pageTop">
-            {!isSidebarOpen && (
-              <>
-                <div onClick={toggleSidebar} className="lb-pageTop_iconWrapper">
-                  <PanelRightOpen size="22px"/>
-                </div>
-                <div className="lb-pageTop_iconWrapper">
-                  <SquarePen size="22px"/>
-                </div>
-              </>
-            )}
-          </div>
-          <div className="lb-pageBody"></div>
+    <div className="lb-page_content">
+      <div className="lb-pageTop">
+        {!isSidebarOpen && (
+          <>
+            <div onClick={toggleSidebar} className="lb-pageTop_iconWrapper">
+              <PanelRightOpen size="22px" />
+            </div>
+            <div className="lb-pageTop_iconWrapper">
+              <SquarePen size="22px" />
+            </div>
+          </>
+        )}{" "}
+        <ModelName name={modelName} content={modelDescription} />
+      </div>
+      <div className="lb-pageBody">
+        <div className="lb-pageBody_innerBox">
+          <ChatMessageWindow
+            chats={chats}
+            streamingData={streamingData}
+            streaming={streaming}
+            messagesEndRef={messagesEndRef}
+            setInputValue={setInputValue}
+          />
+          <ChatInputArea
+            inputValue={inputValue}
+            setInputValue={setInputValue}
+            sendMessage={sendMessage}
+            closeStreaming={closeStreaming}
+            streamingData={streamingData}
+            sendBtnActive={sendBtnActive}
+            aiSuggestions={aiSuggestions}
+          />
         </div>
+      </div>
+    </div>
   );
 };
 
-export default LinkedInBusinessModel
+export default LinkedInBusinessModel;
---
+++ src/hooks/useLIB.js
@@ -0,0 +1,172 @@
+import { modelDetailsMap } from "@/constants/carousel";
+import { useSideBar } from "@/store/sidebarStore";
+import { usePathname } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
+import { usePromptSuggestions } from "./usePromptSuggestion";
+
+
+
+export default function useLIB() {
+  const [inputValue, setInputValue] = useState("");
+  const [sendBtnActive, setSendBtnActive] = useState(false);
+  const [streamingData, setStreamingData] = useState("");
+  const [streaming, setStreaming] = useState(false);
+  const [aiSuggestions, setAISuggestions] = useState([]);
+  const streamingDataRef = useRef("");
+  const eventSourceRef = useRef(null);
+  const messagesEndRef = useRef(null);
+
+
+  const { isSidebarOpen, setIsSidebarOpen } = useSideBar();
+
+  const [modelName, setModelName] = useState("Model");
+  const [modelDescription, setModelDescription] = useState([]);
+
+  const { suggestions } = usePromptSuggestions(inputValue, modelName, modelDescription);
+
+  const chats = []
+
+  const pathname = usePathname();
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+
+
+  useEffect(() => {
+    const segments = pathname.split("/").filter(Boolean);
+    const current = segments[segments.length - 1];
+
+    const model = modelDetailsMap[current];
+    if (model) {
+      setModelName(model.name);
+      setModelDescription(model.description);
+    } else {
+      setModelName("Model");
+      setModelDescription([]);
+    }
+  }, [pathname]);
+
+// imported
+
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [chats]);
+
+  useEffect(() => {
+    setSendBtnActive(inputValue && !streaming);
+  }, [inputValue, streaming]);
+
+
+  const sendMessage = async () => {
+    if (!inputValue || streaming) return;
+
+    // const userChat = {
+    //   sender: "user",
+    //   status: "la_request",
+    //   message: inputValue,
+    //   time: new Date(),
+    // };
+
+    // // Update local state + storage
+    // updateChats(userChat);
+    // // Queue for batched DB write
+    // queueLAChatForDB(userChat);
+
+    // setStreaming(true);
+    // setStreamingData("");
+    // streamingDataRef.current = "";
+
+    // const abortController = new AbortController();
+    // eventSourceRef.current = abortController;
+
+    // queryLegalAssistant(
+    //   inputValue,
+    //   (streamedData) => {
+    //     setStreamingData((prev) => {
+    //       if (prev.endsWith(streamedData)) return prev;
+    //       const updatedData = prev + streamedData;
+    //       streamingDataRef.current = updatedData;
+    //       return updatedData;
+    //     });
+    //   },
+    //   (error) => {
+    //     closeStreaming();
+    //     const errorChat = {
+    //       sender: "bot",
+    //       status: "error",
+    //       message: error?.includes("Unauthorized")
+    //         ? "Unauthorized - Please login"
+    //         : "Server Error - Please try again.",
+    //       time: new Date(),
+    //     };
+    //     updateChats(errorChat);
+    //     queueLAChatForDB(errorChat);
+    //   },
+    //   () => {
+    //     const botChat = {
+    //       sender: "bot",
+    //       status: "la_request",
+    //       message: streamingDataRef.current,
+    //       time: new Date(),
+    //     };
+    //     updateChats(botChat);
+    //     queueLAChatForDB(botChat);
+    //     setStreaming(false);
+    //     setStreamingData("");
+    //   },
+    //   abortController
+    // );
+
+    setInputValue("");
+  };
+
+  const closeStreaming = () => {
+    // if (eventSourceRef.current instanceof AbortController) {
+    //   eventSourceRef.current.abort();
+    //   if (streamingDataRef.current) {
+    //     const botChat = {
+    //       sender: "bot",
+    //       status: "la_request",
+    //       message: streamingDataRef.current,
+    //       time: new Date(),
+    //     };
+
+    //     updateChats(botChat);
+    //     queueLAChatForDB(botChat); // ← Batch this too
+    //   }
+      setStreaming(false);
+      setStreamingData("");
+      streamingDataRef.current = "";
+      eventSourceRef.current = null;
+    // }
+  };
+
+// imported end
+
+useEffect(() => {
+  if(suggestions && suggestions?.length > 0) {
+    setAISuggestions(suggestions)
+  }
+
+}, [suggestions])
+
+  return {
+    isSidebarOpen,
+    toggleSidebar,
+    modelName,
+    modelDescription,
+    inputValue,
+    setInputValue,
+    sendMessage,
+    closeStreaming,
+    streamingData,
+    streaming,
+    sendBtnActive,
+    chats,
+    messagesEndRef,
+    aiSuggestions
+  };
+}
