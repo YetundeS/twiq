1cdb27b1b2a6bfafc9e3a483ac2185873c49e768
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2025-05-23 19:49:16 +0100

    fixed linkedin personal model, ready for backend integration

--- src/app/platform/[slug]/linkedin-personal/page.jsx
+++ src/app/platform/[slug]/linkedin-personal/page.jsx
@@ -1,46 +1,67 @@
 "use client";
 
-import { useSideBar } from "@/store/sidebarStore";
-import './lp.css';
-import useAuthStore from "@/store/authStore";
-import { useEffect } from "react";
-import { useHydrationZustand } from "@codebayu/use-hydration-zustand";
+import "./lp.css";
 import { PanelRightOpen, SquarePen } from "lucide-react";
+import useLIP from "@/hooks/useLIP";
+import ChatInputArea from "@/components/carouselComponents/chatInputArea";
+import ChatMessageWindow from "@/components/carouselComponents/chatMessageWindow";
+import ModelName from "@/components/modelsComponent/modelName";
 
 const LinkedInPersonalModel = () => {
-  const { isSidebarOpen, setIsSidebarOpen } = useSideBar();
-
-  const { user } = useAuthStore();
-
-  const isHydrated = useHydrationZustand(useAuthStore);
-
-  useEffect(() => {
-    if (isHydrated && !user) { 
-      router.push("/auth");
-    }
-  }, [user, isHydrated]);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
+  const {
+    isSidebarOpen,
+    toggleSidebar,
+    modelName,
+    modelDescription,
+    inputValue,
+    setInputValue,
+    sendMessage,
+    closeStreaming,
+    streamingData,
+    streaming,
+    sendBtnActive,
+    chats,
+    messagesEndRef,
+    aiSuggestions,
+  } = useLIP();
 
   return (
-        <div className="lp-page_content">
-          <div className="lp-pageTop">
-            {!isSidebarOpen && (
-              <>
-                <div onClick={toggleSidebar} className="lp-pageTop_iconWrapper">
-                  <PanelRightOpen size="22px"/>
-                </div>
-                <div className="lp-pageTop_iconWrapper">
-                  <SquarePen size="22px"/>
-                </div>
-              </>
-            )}
-          </div>
-          <div className="lp-pageBody"></div>
+    <div className="lp-page_content">
+      <div className="lp-pageTop">
+        {!isSidebarOpen && (
+          <>
+            <div onClick={toggleSidebar} className="lp-pageTop_iconWrapper">
+              <PanelRightOpen size="22px" />
+            </div>
+            <div className="lp-pageTop_iconWrapper">
+              <SquarePen size="22px" />
+            </div>
+          </>
+        )}
+        <ModelName name={modelName} content={modelDescription} />
+      </div>
+      <div className="lp-pageBody">
+        <div className="lp-pageBody_innerBox">
+          <ChatMessageWindow
+            chats={chats}
+            streamingData={streamingData}
+            streaming={streaming}
+            messagesEndRef={messagesEndRef}
+            setInputValue={setInputValue}
+          />
+          <ChatInputArea
+            inputValue={inputValue}
+            setInputValue={setInputValue}
+            sendMessage={sendMessage}
+            closeStreaming={closeStreaming}
+            streamingData={streamingData}
+            sendBtnActive={sendBtnActive}
+            aiSuggestions={aiSuggestions}
+          />
         </div>
+      </div>
+    </div>
   );
 };
 
-export default LinkedInPersonalModel
+export default LinkedInPersonalModel;
