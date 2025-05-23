47a00f3ee3ed2fb5fdf350327f73f09ed24c1020
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2025-05-23 19:55:06 +0100

    fixed video script model page, ready for backend integration

--- src/app/platform/[slug]/video-scripts/page.jsx
+++ src/app/platform/[slug]/video-scripts/page.jsx
@@ -1,46 +1,67 @@
 "use client";
 
-import { useSideBar } from "@/store/sidebarStore";
-import './video-scripts.css';
-import useAuthStore from "@/store/authStore";
-import { useEffect } from "react";
-import { useHydrationZustand } from "@codebayu/use-hydration-zustand";
+import "./video-scripts.css";
 import { PanelRightOpen, SquarePen } from "lucide-react";
+import useVSM from "@/hooks/useVSM";
+import ChatInputArea from "@/components/carouselComponents/chatInputArea";
+import ChatMessageWindow from "@/components/carouselComponents/chatMessageWindow";
+import ModelName from "@/components/modelsComponent/modelName";
 
 const VideoScriptsModel = () => {
-  const { isSidebarOpen, setIsSidebarOpen } = useSideBar();
-
-  const { user } = useAuthStore();
-
-  const isHydrated = useHydrationZustand(useAuthStore);
-
-  useEffect(() => {
-    if (isHydrated && !user) { 
-      router.push("/auth");
-    }
-  }, [user, isHydrated]);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
+  const {
+    isSidebarOpen,
+    toggleSidebar,
+    modelName,
+    modelDescription,
+    inputValue,
+    setInputValue,
+    sendMessage,
+    closeStreaming,
+    streamingData,
+    streaming,
+    sendBtnActive,
+    chats,
+    messagesEndRef,
+    aiSuggestions,
+  } = useVSM();
 
   return (
-        <div className="vs-page_content">
-          <div className="vs-pageTop">
-            {!isSidebarOpen && (
-              <>
-                <div onClick={toggleSidebar} className="vs-pageTop_iconWrapper">
-                  <PanelRightOpen size="22px"/>
-                </div>
-                <div className="vs-pageTop_iconWrapper">
-                  <SquarePen size="22px"/>
-                </div>
-              </>
-            )}
-          </div>
-          <div className="vs-pageBody"></div>
+    <div className="vs-page_content">
+      <div className="vs-pageTop">
+        {!isSidebarOpen && (
+          <>
+            <div onClick={toggleSidebar} className="vs-pageTop_iconWrapper">
+              <PanelRightOpen size="22px" />
+            </div>
+            <div className="vs-pageTop_iconWrapper">
+              <SquarePen size="22px" />
+            </div>
+          </>
+        )}
+        <ModelName name={modelName} content={modelDescription} />
+      </div>
+      <div className="vs-pageBody">
+        <div className="vs-pageBody_innerBox">
+          <ChatMessageWindow
+            chats={chats}
+            streamingData={streamingData}
+            streaming={streaming}
+            messagesEndRef={messagesEndRef}
+            setInputValue={setInputValue}
+          />
+          <ChatInputArea
+            inputValue={inputValue}
+            setInputValue={setInputValue}
+            sendMessage={sendMessage}
+            closeStreaming={closeStreaming}
+            streamingData={streamingData}
+            sendBtnActive={sendBtnActive}
+            aiSuggestions={aiSuggestions}
+          />
         </div>
+      </div>
+    </div>
   );
 };
 
-export default VideoScriptsModel
+export default VideoScriptsModel;
